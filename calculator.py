ed64ae8969799613d61ac7b6df7a7f80b7ebef31
Mohit Sharma <200160784+mohit716@users.noreply.github.com>
2025-12-05 22:25:41 -0500

    WIP: calculator API

--- calculator.py
+++ calculator.py
@@ -1,12 +1,15 @@
+from fastapi import FastAPI
+app = FastAPI()
+@app.post("post/add")-> float:
 def add(a:float, b:float)->float:
     return a + b
-
+@app.post("/subtract", parameters={"a": float, "b": float})
 def subtract(a:float, b:float)->float:
     return a - b
-
+@app.post("/multiply", parameters={"a": float, "b": float})
 def multiply(a:float, b:float)->float:
     return a * b
-
+@app.post("/divide", parameters={"a": float, "b": float})
 def divide(a:float, b:float)->float:
     return a / b
 
